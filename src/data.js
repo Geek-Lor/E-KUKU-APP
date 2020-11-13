da093987a0d7eaa3ea3a9e62b3d33015072a2a75
Timothy Otieno <timothyking094@gmail.com>
2020-11-13 11:52:07 +0300

    Temporary data container

---
+++ src/data.js
@@ -0,0 +1,176 @@
+import hen1 from './img/hen/hen_1.jpg';
+import hen2 from './img/hen/hen_2.jpg';
+import hen3 from './img/hen/hen_3.jpg';
+import hen4 from './img/hen/hen_4.jpg';
+import hen5 from './img/hen/hen_5.jpg';
+import hen6 from './img/hen/hen_6.jpg';
+import hen7 from './img/hen/hen_7.jpg';
+import hen8 from './img/hen/hen_8.jpg';
+import rooster1 from './img/rooster/rooster_1.jpg';
+import rooster2 from './img/rooster/rooster_2.jpg';
+import rooster3 from './img/rooster/rooster_3.jpg';
+import rooster4 from './img/rooster/rooster_4.jpg';
+import rooster5 from './img/rooster/rooster_5.jpg';
+import rooster6 from './img/rooster/rooster_6.jpg';
+import rooster7 from './img/rooster/rooster_7.jpg';
+import egg1 from './img/eggs/eggs_4.jpg';
+import egg3 from './img/eggs/eggs_3.jpg';
+
+
+const data = [
+    {
+        id:1,
+        img: rooster1,
+        product: 'Rooster',
+        productName:'Kienyeji Rooster',
+        type: 'kienyeji',
+        price: 1200,
+        description: 'An Eight-month Kienyeji Rooster'
+    },
+    {
+        id:2,
+        img: rooster2,
+        product:'Rooster',
+        productName:'Grade Rooster',
+        type: 'grade',
+        price: 1000,
+        description: 'An Four-month Grade Rooster'
+    },
+    {
+        id:3,
+        img: rooster3,
+        product: 'Rooster',
+        productName:'Kienyeji Rooster',
+        type: 'kienyeji',
+        price: 800,
+        description: 'An Six-month Kienyeji Rooster'
+    },
+    {
+        id:4,
+        img: rooster4,
+        product: 'Rooster',
+        productName:'Grade Rooster',
+        type: 'grade',
+        price: 600,
+        description: 'An Two-month Grade Rooster'
+    },
+    {
+        id:5,
+        img: rooster5,
+        product: 'Rooster',
+        productName:'Kienyeji Rooster',
+        type: 'kienyeji',
+        price: 500,
+        description: 'An Three-month Kienyeji Rooster'
+    },
+    {
+        id:6,
+        img: rooster6,
+        product: 'Rooster',
+        productName:'Grade Rooster',
+        type: 'kienyeji',
+        price: 400,
+        description: 'An One and a half Grade Rooster'
+    },
+    {
+        id:7,
+        img: rooster7,
+        product:'Rooster',
+        productName: 'Kienyeji Rooster',
+        type: 'kienyeji',
+        price: 700,
+        description: 'An Five-month Kienyeji rooster'
+    },
+    {
+        id:8,
+        img: hen1,
+        product:'Hen',
+        productName: 'Kienyeji Hen',
+        type: 'kienyeji',
+        price: 1000,
+        description: 'An Eight-month Kienyeji Hen'
+    },
+    {
+        id:9,
+        img: hen2,
+        product:'Hen',
+        productName: 'Grade Hen',
+        type: 'grade',
+        price: 900,
+        description: 'An Six-month Grade Hen'
+    },
+    {
+        id:10,
+        img: hen3,
+        product:'Hen',
+        productName: 'Kienyeji Hen',
+        type: 'kienyeji',
+        price: 800,
+        description: 'An Six-month Kienyeji Hen'
+    },
+    {
+        id:11,
+        img:hen8,
+        product:'Hen',
+        productName: 'Grade Hen',
+        type: 'grade',
+        price: 700,
+        description: 'An Four-month Grade Hen'
+    },
+    {
+        id:12,
+        img: hen5,
+        product:'Hen',
+        productName: 'Kienyeji Hen',
+        type: 'kienyeji',
+        price: 600,
+        description: 'A Three-month Kienyeji Hen'
+    },
+    {
+        id:13,
+        img: hen6,
+        product:'Hen',
+        productName: 'Grade Hen',
+        type: 'grade',
+        price: 600,
+        description: 'A Two-month Grade Hen'
+    },
+    {
+        id:14,
+        img: hen7,
+        product:'Hen',
+        productName: 'Grade Hen',
+        type: 'grade',
+        price: 500,
+        description: 'A One and a half month Grade Hen'
+    },
+    {
+        id:15,
+        img:  hen4,
+        product:'Hen',
+        productName: 'Kienyeji Hen',
+        type: 'kienyeji',
+        price: 500,
+        description: 'A Two month Kienyeji Hen'
+    },
+    {
+        id:16,
+        img: egg1,
+        product:'Eggs',
+        productName: 'Kienyeji Eggs',
+        type: 'kienyeji',
+        price: 420,
+        description: 'One Tray of Kienyeji Hen Eggs'
+    },
+    {
+        id:17,
+        img: egg3,
+        product:'Eggs',
+        productName: 'Grade Eggs',
+        type: 'grade',
+        price: 300,
+        description: 'One Tray of Grade Hen Eggs'
+    }
+]
+
+export default data;
