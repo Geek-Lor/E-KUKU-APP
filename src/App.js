9677ab138f790a23322bda9ccda023950fec2171
Timothy Otieno <timothyking094@gmail.com>
2020-11-13 11:51:02 +0300

    Main App Component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
-/**Styling Import */
+/**Styling */
 import './App.css';
+
+/**Routing */
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+/**Global state */
+import ProductProvider from './context/product-context/productProvider';
 
 /**Components Import */
 import Navbar from './components/Navbar';
-// import Home from './pages/Home';
-import Market from './pages/Market'
+import Home from './pages/Home';
+import Market from './pages/Market';
+import Cart from './pages/Cart';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
   <>
-   <Navbar />
-   {/* <Home /> */}
-    <Market />
+  <ProductProvider>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route path='/market'>
+          <Market />
+        </Route>
+        <Route path='/cart'>
+          <Cart />
+        </Route>
+        <Route path='/checkout'>
+          <Checkout />
+        </Route>
+      </Switch>
+    </Router>
+  </ProductProvider> 
   </>
   );
 }
